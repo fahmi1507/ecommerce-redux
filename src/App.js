227e689209bdc3b6c74beb165c0562014828df16
fahmi-1997 <fahmialamsyah97@gmail.com>
2021-08-18 13:32:57 +0700

    base styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 function App() {
   return (
-    <div className="App">
-      <p>heuhuhllo</p>
+    <div className="grid-container">
+      <header>
+        <a href="/">Shopping Cart</a>
+      </header>
+      <main>Product List</main>
+      <footer>Copyright 2021</footer>
     </div>
   );
 }
